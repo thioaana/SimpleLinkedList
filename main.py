4a219d382cc65f3277758e48fabdafc76bb3ce65
makis <54209831+thioaana@users.noreply.github.com>
2024-01-09 14:56:09 +0000

    printList function completed

--- main.py
+++ main.py
@@ -15,10 +15,10 @@ class Node:
     def getNext(self):
         return self.__next
 
-    def setData(new_data):
+    def setData(self, new_data):
         self.__data = new_data
-    
-    def setNext(new-next):
+
+    def setNext(self, new_next):
         self.__next = new_next
 
 
@@ -26,44 +26,56 @@ class LinkedList:
     def __init__(self):
         self.__head = None
 
-    def isEmpty():
+    def isEmpty(self):
         return self.__head is None
 
-    def size():
+    def size(self):
         count = 0
         cur_node = self.__head
 
         while cur_node is not None:
             count += 1
             cur_node = cur_node.getNext()
-        
+
         return count
 
-    def push(new_data):
+    def push(self, new_data):
         """
         Insert a new node in the beggining of the list
         """
         new_node = Node(new_data)
-        new_node.setNext = self.__head
+        new_node.setNext(self.__head)
         self.__head = new_node
 
-    def pop():
+    def pop(self):
         """
         Return the header node and move the Head to the next node
         """
-        if self.isEmpty() :
+        if self.isEmpty():
             return None
 
         initial_node = self.__head
         self.__head = initial_node.getNext()
         return initial
-        
 
-        
-
+    def printList(self):
+        current = self.__head
+        while current is not None:
+            print(current.getData())
+            current = current.getNext()
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    pass
+    myList = LinkedList()
+    print(f'Is Linked List empty ? {myList.isEmpty()}')
+
+    myList.push('A')
+    myList.printList()
+    myList.push('B')
+    print(f'Is Linked List empty ? {myList.isEmpty()}')
+    myList.printList()
+    myList.push('C')
+    myList.printList()
+    print(myList.size())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
