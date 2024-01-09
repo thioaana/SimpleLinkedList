40dbafe88fed31e7f9bd75e93631da7634010929
makis <54209831+thioaana@users.noreply.github.com>
2024-01-09 07:53:49 +0000

    Started main.py

--- main.py
+++ main.py
@@ -4,13 +4,66 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f"Hi, {name}")  # Press Ctrl+F8 to toggle the breakpoint.
+class Node:
+    def __init__(self, data):
+        self.__data = data
+        self.__next = None
+
+    def getData(self):
+        return self.__data
+
+    def getNext(self):
+        return self.__next
+
+    def setData(new_data):
+        self.__data = new_data
+    
+    def setNext(new-next):
+        self.__next = new_next
+
+
+class LinkedList:
+    def __init__(self):
+        self.__head = None
+
+    def isEmpty():
+        return self.__head is None
+
+    def size():
+        count = 0
+        cur_node = self.__head
+
+        while cur_node is not None:
+            count += 1
+            cur_node = cur_node.getNext()
+        
+        return count
+
+    def push(new_data):
+        """
+        Insert a new node in the beggining of the list
+        """
+        new_node = Node(new_data)
+        new_node.setNext = self.__head
+        self.__head = new_node
+
+    def pop():
+        """
+        Return the header node and move the Head to the next node
+        """
+        if self.isEmpty() :
+            return None
+
+        initial_node = self.__head
+        self.__head = initial_node.getNext()
+        return initial
+        
+
+        
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
-    print_hi("PyCharm")
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
